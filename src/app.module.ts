caacfe39b1ef5e910cc86ab31b4e4221406fda04
Derick Darlan <derickdarlan304@gmail.com>
2025-08-05 11:31:50 -0300

    Inicial commit

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+
+@Module({
+  imports: [
+     MailerModule.forRoot({
+      transport: 'smtps://wendell.prosacco@ethereal.email:ZKuuyBEzBSbPcpb26f@smtp.ethereal.email',
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
